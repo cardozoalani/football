c402310cc56741c5ac4295c20c8a6bde9c7dcdd5
Alan Cardozo <cardozoalani@hotmail.com>
2023-12-05 20:43:04 -0300

    feat(backend): add endpoint get all teams

--- backend/src/application/team/TeamService.ts
+++ backend/src/application/team/TeamService.ts
@@ -9,9 +9,18 @@ class TeamService {
 
   async getTeamById(teamId: string) {
     try {
-      console.log("Calling getTeamById in TeamService");
       const team = await this.teamRepository.findById(teamId);
       console.log("Team retrieved:", team);
+      return team;
+    } catch (error) {
+      console.error("Error in getTeamById in TeamService:", error);
+      throw error;
+    }
+  }
+  async getTeams() {
+    try {
+      const team = await this.teamRepository.find();
+      console.log("Teams retrieved:", team);
       return team;
     } catch (error) {
       console.error("Error in getTeamById in TeamService:", error);
