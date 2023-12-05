fd4519f577870f0e6933fa5d874d4e0667ca4260
Alan Cardozo <cardozoalani@hotmail.com>
2023-12-05 17:45:20 -0300

    feat(backend): initialize app

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import { app } from "./infrastructure/express/server";
+import { startFetchDataCron } from "./infrastructure/cron/fetchDataCron.ts";
+import dotenv from "dotenv";
+dotenv.config();
+const start = async () => {
+  console.log("Starting application...\n");
+  if (!process.env.DB_USER) {
+    throw new Error("DB_USER must be defined");
+  }
+  if (!process.env.DB_HOST) {
+    throw new Error("DB_HOST must be defined");
+  }
+  if (!process.env.DB_NAME) {
+    throw new Error("DB_NAME must be defined");
+  }
+  if (!process.env.DB_PASSWORD) {
+    throw new Error("DB_PASSWORD must be defined");
+  }
+  if (!process.env.DB_PORT) {
+    throw new Error("DB_PORT must be defined");
+  }
+  if (!process.env.API_KEY) {
+    throw new Error("API_KEY must be defined");
+  }
+
+  try {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listening on port number ${process.env.PORT || 3000}`);
+    });
+    startFetchDataCron();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
