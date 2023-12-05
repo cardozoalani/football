50ed3b1648d0ddde38ee4e6430844a16131da7cb
Alan Cardozo <cardozoalani@hotmail.com>
2023-12-05 20:43:16 -0300

    fix(backend): configure cors

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import TeamController from "../../presentation/controllers/TeamController";
 import MatchController from "../../presentation/controllers/MatchController";
@@ -10,15 +11,16 @@ import MatchService from "../../application/match/MatchService";
 dotenv.config();
 
 const app: Application = express();
-
-app.use(express.json({ limit: '10mb' })); // Adjust the limit as needed
-app.use(express.urlencoded({ limit: '10mb', extended: true })); // Adjust the limit as needed
+app.use(cors())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send({ status: "success", message: "Football API" });
 });
 const teamService = new TeamService();
 const teamController = new TeamController(teamService);
 app.get("/teams/:teamId", teamController.getTeamById);
+app.get("/teams", teamController.getTeams);
 
 const playersService = new PlayersService()
 const playersController = new PlayersController(playersService);
